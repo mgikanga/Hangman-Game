2f3c470600995fa38d7e4ef9f837596291d31f6f
Monicah Gikanga <mgikanga@Admins-MacBook-Air.local>
2018-03-10 09:19:05 -0800

    linked my javascript and css files

---
+++ assets/javascript/game.js
@@ -0,0 +1,6 @@
+// listen to user press a key.
+document.onkeyup = function(event) {
+    var userKey = String.fromCharCode(event.which).toLowerCase();
+        console.log(userKey);
+    } 
+// start the game
